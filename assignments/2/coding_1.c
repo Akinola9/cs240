c284e32d47cc5b60ea878db083c89aa593c707ec
Akinola9 <92336729+Akinola9@users.noreply.github.com>
2023-03-20 23:10:39 -0400

    Update coding_1.c

--- assignments/2/coding_1.c
+++ assignments/2/coding_1.c
@@ -13,21 +13,45 @@
 // scanf  will treat the string as (white space and new line seprated) words
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, char *argv[]) {
-    int T, N, D, C, M;
-    char s[10001]; // array is not resize
-
-//    while (scanf("%s", s) == -1)
-
-    scanf("%d", &T);
-    printf("the %d test cases\n", T);
-
-    for (int i = 1; i <= T; ++i) {
-        scanf("%d %d %d %d %s", &N, &D, &C, &M, s);
-        printf("#%d sum is %d, the string is %s\n", i, N + D + C + M, s);
+int main() {
+    int T;
+    cin >> T;
+    for (int t = 1; t <= T; t++) {
+        int N, D, C, M;
+        cin >> N >> D >> C >> M;
+        string S;
+        cin >> S;
+        bool all_dogs_fed = true;
+        int remaining_dog_food = D;
+        int remaining_cat_food = C;
+        for (int i = 0; i < N; i++) {
+            if (S[i] == 'C') {
+                if (remaining_cat_food > 0) {
+                    remaining_cat_food--;
+                } else {
+                    all_dogs_fed = false;
+                    break;
+                }
+            } else { // S[i] == 'D'
+                if (remaining_dog_food > 0) {
+                    remaining_dog_food--;
+                    remaining_cat_food += M;
+                } else {
+                    all_dogs_fed = false;
+                    break;
+                }
+            }
+        }
+        cout << "Case #" << t << ": ";
+        if (all_dogs_fed) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
     }
-
-
+    return 0;
 }
